76ad353952e60427e104b5401f6a7c2972939a43
igor.pashnin <pshnn@protonmail.com>
2020-11-07 18:12:51 +0200

    Update fetching of geolocation
    - add fetching geolocation by button press.

--- public/index.js
+++ public/index.js
@@ -1,16 +1,27 @@
-console.log("Here we are");
-
-const logCoords = (position) => {
-  console.log("Lat:", position.coords.latitude);
-  console.log("Lon:", position.coords.longitude);
+const getFirstElementByClassName = (className) => {
+  return document.getElementsByClassName(className)[0];
 };
 
-const populateCoordinateInputs = (position) => {
-  //console.log(document.getElementsByClassName("js-latitude"));
-  document.getElementsByClassName("js-latitude")[0].value = position.coords.latitude;
-  document.getElementsByClassName("js-longitude")[0].value = position.coords.longitude;
+const getLocationButton = getFirstElementByClassName("js-get-location-btn");
+
+const getLocation = () => {
+  const geo = navigator.geolocation;
+
+  if (geo) {
+    geo.getCurrentPosition(
+      (location) => {
+        const latitudeInput = getFirstElementByClassName("js-latitude");
+        const longitudeInput = getFirstElementByClassName("js-longitude");
+
+        latitudeInput.value = location.coords.latitude;
+        longitudeInput.value = location.coords.longitude;
+      },
+      (error) => {
+        const errorMessage = error.message;
+        console.log("errorMessage::", errorMessage);
+      }
+    );
+  }
 };
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(populateCoordinateInputs);
-}
+getLocationButton.addEventListener("click", getLocation);
