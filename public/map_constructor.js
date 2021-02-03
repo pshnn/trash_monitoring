3dd5c8f3b5000b7cefee6641125b47e0e9587c84
Igor <20119307+pshnn@users.noreply.github.com>
2021-02-04 01:59:53 +0200

    Update JS Map class (#10)
    - remove unnecessary attributes from constructor;
    - add popup for markers.
    
    Co-authored-by: igor.pashnin <pshnn@protonmail.com>

--- public/map_constructor.js
+++ public/map_constructor.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = "pk.eyJ1IjoiZW1lbGRtb24iLCJhIjoiY2tobWhmMWpnMGZtejJzazFidWE5eHZ2eSJ9.fZilTg2I6AyrsFZ9ElV8VQ";
 
 export class Map {
-  constructor({ container, zoom, center, setupControls }) {
+  constructor({ container, zoom, center }) {
     this.map = new mapboxgl.Map({
       container: container,
       style: 'mapbox://styles/mapbox/streets-v11',
@@ -15,12 +15,21 @@ export class Map {
         trackUserLocation: true
       })
     );
+
+    this.markers = [];
   }
 
   addMarker({ latitude, longitude }) {
+    const popup = new mapboxgl
+      .Popup()
+      .setHTML(
+        `<a href="/location?latitude=${latitude}&longitude=${longitude}">View</a>`
+      );
+
     new mapboxgl
       .Marker()
       .setLngLat([longitude, latitude])
+      .setPopup(popup)
       .addTo(this.map);
   }
 }
