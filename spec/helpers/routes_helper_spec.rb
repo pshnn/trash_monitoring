482e0297f984970c71cb9aa6195ed149248047ce
igor.pashnin <pshnn@protonmail.com>
2021-02-28 02:09:47 +0200

    Add specs for RoutesHelper

---
+++ spec/helpers/routes_helper_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+describe RoutesHelper do
+  describe '.root_path' do
+    it 'returns proper root path' do
+      expect(described_class.root_path).to eq '/'
+    end
+  end
+
+  describe '.map_path' do
+    it 'returns proper map path' do
+      expect(described_class.map_path).to eq '/map'
+    end
+  end
+
+  describe '.edit_location_path' do
+    context 'when coordinates were passed' do
+      it 'returns proper edit location path' do
+        latitude = '34.1234567'
+        longitude = '87.1234567'
+
+        expect(
+          described_class.edit_location_path(
+            latitude: latitude,
+            longitude: longitude
+          )
+        ).to(
+          eq("/locations/edit?latitude=#{latitude}&longitude=#{longitude}")
+        )
+      end
+    end
+
+    context 'when coordinates were not passed' do
+      it 'returns proper edit location path' do
+        expect(described_class.edit_location_path).to eq '/locations/edit'
+      end
+    end
+  end
+
+  describe '.delete_location_path' do
+    context 'when coordinates were passed' do
+      it 'returns proper edit location path' do
+        latitude = '34.1234567'
+        longitude = '87.1234567'
+
+        expect(
+          described_class.delete_location_path(
+            latitude: latitude,
+            longitude: longitude
+          )
+        ).to(
+          eq("/locations?latitude=#{latitude}&longitude=#{longitude}")
+        )
+      end
+    end
+
+    context 'when coordinates were not passed' do
+      it 'returns proper edit location path' do
+        expect(described_class.delete_location_path).to eq '/locations'
+      end
+    end
+  end
+end
