2f346aab0825cd1d24b065b8285a32f73e4fe11b
pshnn <pshnn@protonmail.com>
2021-05-28 23:49:05 +0300

    Add specs for NewLocationValidator

---
+++ spec/validators/new_location_validator_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+describe NewLocationValidator do
+  describe '.validate' do
+    let(:valid_data) { Struct.new(:latitude, :longitude).new('43.38', '87.34') }
+
+    context 'when valid data passed' do
+      it 'returns object with no errors' do
+        expect(described_class.validate(valid_data).errors.count).to eq 0
+      end
+    end
+
+    context 'when data passed with float like latitude' do
+      let(:data) { Struct.new(:latitude, :longitude).new('43.38', '87.34') }
+
+      it 'returns object with no errors' do
+        [34.32453, 23, '34', '14.23425234'].each do |latitude|
+          expect(described_class.validate(
+            Struct.new(:latitude, :longitude).new(latitude, '87.34')
+          ).errors.count).to eq 0
+        end
+      end
+    end
+
+    context 'when data passed with not float like latitude' do
+      let(:data) { Struct.new(:latitude, :longitude).new('sd687s6f7', '87.34') }
+
+      it 'returns object with errors' do
+        expect(described_class.validate(data).errors.count).to be > 0
+      end
+    end
+
+    context 'when data passed with no latitude' do
+      let(:data) do
+        valid_data.latitude = nil
+
+        valid_data
+      end
+
+      it 'returns object with errors' do
+        expect(described_class.validate(data).errors.count).to be > 0
+      end
+
+      it 'returns object with proper error message' do
+        expect(described_class.validate(data).errors).to(
+          include('Latitude must be present')
+        )
+      end
+    end
+
+    context 'when data passed with no longitude' do
+      let(:data) do
+        valid_data.longitude = nil
+
+        valid_data
+      end
+
+      it 'returns object with errors' do
+        expect(described_class.validate(data).errors.count).to be > 0
+      end
+
+      it 'returns object with proper error message' do
+        expect(described_class.validate(data).errors).to(
+          include('Longitude must be present')
+        )
+      end
+    end
+  end
+end
