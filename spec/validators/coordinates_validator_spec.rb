e25efc2e3081575c58fea19359944360e13c1871
pshnn <pshnn@protonmail.com>
2021-10-16 13:42:25 +0300

    Add specs for CoordinatesValidator

--- spec/validators/coordinates_validator_spec.rb
+++ spec/validators/coordinates_validator_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-describe NewLocationValidator do
+require 'spec_helper'
+
+describe CoordinatesValidator do
   describe '.validate' do
     let(:valid_data) { Struct.new(:latitude, :longitude).new('43.38', '87.34') }
 
@@ -14,7 +16,7 @@ describe NewLocationValidator do
       let(:data) { Struct.new(:latitude, :longitude).new('43.38', '87.34') }
 
       it 'returns object with no errors' do
-        [34.32453, 23, '34', '14.23425234'].each do |latitude|
+        [34.32453, 23.0, '34.0', '14.23424'].each do |latitude|
           expect(described_class.validate(
             Struct.new(:latitude, :longitude).new(latitude, '87.34')
           ).errors.count).to eq 0
@@ -63,6 +65,26 @@ describe NewLocationValidator do
         expect(described_class.validate(data).errors).to(
           include('Longitude must be present')
         )
+      end
+    end
+
+    context 'when data passed with not float like longitude' do
+      let(:data) { Struct.new(:latitude, :longitude).new('87.34', 'sd687s6f7') }
+
+      it 'returns object with errors' do
+        expect(described_class.validate(data).errors.count).to be > 0
+      end
+    end
+
+    context 'when data passed with float like longitude' do
+      let(:data) { Struct.new(:latitude, :longitude).new('43.38', '87.34') }
+
+      it 'returns object with no errors' do
+        [34.32453, 23.0, '34.0', '14.23424'].each do |coordinate|
+          expect(described_class.validate(
+            Struct.new(:latitude, :longitude).new('87.34', coordinate)
+          ).errors.count).to eq 0
+        end
       end
     end
   end
